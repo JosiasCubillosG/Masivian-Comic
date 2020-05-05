3bb213d39bc3dbc9202f16eb6368df25e8b18ce3
Josias Cubillos Gutierrez <josiascubillosg@gmail.com>
2020-05-05 10:44:23 -0500

    Se realizo el consumo de la api y se comenzo a estructurar la parte visual

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
+import Header from './Header.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faStar} from '@fortawesome/free-solid-svg-icons'
 
 class App extends React.Component {
 
@@ -9,14 +12,15 @@ class App extends React.Component {
 
     componentDidMount = async() => {
 
-        Axios.get('https://xkcd.com/614/info.0.json',{
-            headers: {"Access-Control-Allow-Origin": true}
-        })
+        Axios.get('https://xkcd.com/614/info.0.json')
         .then(res =>{
-            console.log(res)
-            if(res.data.status === 'success') {
-                console.log(res)
+            if(res.statusText === 'OK') {
+                this.setState({
+                    comic: res.data
+                })
+                console.log(res.data)
             }else{
+                console.log('error else')
                 const error = new Error(res.error)
                 throw error
             }
@@ -26,12 +30,41 @@ class App extends React.Component {
         })
     }
 
+    handleStars = (e) => {
+    }
+
 
     render(){
+        const {comic} = this.state
         return(
-            <div>
-                H
-            </div>
+            <React.Fragment>
+                <Header />
+                <div className="comic">
+                    <div className="comic__title">
+                        <h2>{comic.safe_title}</h2>
+                    </div>
+                    <div className="comic__image">
+                        <img className="comic__image__img" src={comic.img} />
+                    </div>
+                    <div className="comic__stars">
+                        <button onClick={this.handleStars()}>
+                            <FontAwesomeIcon className="comic__stars__star" icon={faStar} size="3x" />
+                        </button>
+                        <button onClick={this.handleStars()}>
+                            <FontAwesomeIcon className="comic__stars__star" icon={faStar} size="3x" />
+                        </button>
+                        <button onClick={this.handleStars()}>
+                            <FontAwesomeIcon className="comic__stars__star" icon={faStar} size="3x" />
+                        </button>
+                        <button onClick={this.handleStars()}>
+                            <FontAwesomeIcon className="comic__stars__star" icon={faStar} size="3x" />
+                        </button>
+                        <button onClick={this.handleStars()}>
+                            <FontAwesomeIcon className="comic__stars__star" icon={faStar} size="3x" />
+                        </button>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
